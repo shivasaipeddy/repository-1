04093b0e5848a883695edf8f2ad54725fcc712ae
shivasaipeddy <85818546+shivasaipeddy@users.noreply.github.com>
2022-06-28 13:18:02 +0530

    Add files via upload

---
+++ src/main/java/hibernateproject/webapp/App.java
@@ -0,0 +1,13 @@
+package hibernateproject.webapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
